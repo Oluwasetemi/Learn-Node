111d875d11a19d837e5f8dc44ee609912124ecdf
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-04-27 13:06:30 +0100

    create the /add route and store.js data model

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,9 @@
 exports.homePage = (req, res) => {
-    res.render('index')
+    console.log(req.name)
+    res.render('index', {name: req.name})
+}
+
+exports.homePage = (req, res) => {
+    console.log(req.name)
+    res.render('index', {name: req.name})
 }
