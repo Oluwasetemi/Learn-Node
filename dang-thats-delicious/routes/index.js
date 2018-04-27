cc03706f089dc8c6c3e6fae9fc4bf6a67549e0aa
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-04-27 11:29:26 +0100

    add storeController.js to controller and create index.pug

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
 // Do work here
-router.get('/', (req, res) => {
-  res.render('hello');
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('')
