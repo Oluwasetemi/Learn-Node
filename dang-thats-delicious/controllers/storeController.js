19ca6652845fedcf9ef1ef3705d3bce05c55c041
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-04-28 07:22:01 +0100

    add the update route. I am loving this

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,17 +2,15 @@ const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
 exports.homePage = (req, res) => {
-    req.flash('warning', `a very fatal error`)
-    req.flash('error', `a very fatal error`)
-    req.flash('info', `a very fatal error`)
-    req.flash('success', `a very fatal error`)
-    req.flash('me  away', `a very fatal error`)
-    req.flash('info', `a very fatal error`)
-    res.render('index', {name: req.name})
+    res.render('index', {
+        name: req.name
+    })
 }
 
 exports.addStore = (req, res) => {
-    res.render('editStore', { title: 'Add Store' })
+    res.render('editStore', {
+        title: 'Add Store'
+    })
 }
 
 exports.createStore = async (req, res) => {
@@ -24,14 +22,32 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
     const stores = await Store.find()
     console.log(stores)
-    res.render('stores', { title: 'Stores', stores })
+    res.render('stores', {
+        title: 'Stores',
+        stores
+    })
 }
 
 exports.editStore = async (req, res) => {
-  // 1. find the store given the ID
-    const store = await Store.findOne({ _id: req.params.id })
-  // 2. confirm they are the owner of the store
-    res.render('editStore', { title: `Edit ${store.name}`, store })
-  // 3. Render out the edit form so the user can update their store
+    // 1. find the store given the ID
+    const store = await Store.findOne({
+        _id: req.params.id
+    })
+    // 2. confirm they are the owner of the store
+    res.render('editStore', {
+        title: `Edit ${store.name}`,
+        store
+    }) 
+    // 3. Render out the edit form so the user can update their store
 }
 
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, 
+    {
+        new: true,
+        runValidators: true
+    }).exec()
+    console.log(store)
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`)
+    res.redirect(`/stores/${store._id}/edit`)
+}
