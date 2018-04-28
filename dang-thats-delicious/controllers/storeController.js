0b78be82c3830e54e552e028e52b45a267f55340
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-04-28 06:07:29 +0100

    create stores.pug and _storeCard.pug mixin. working perfectly

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,4 +1,13 @@
+const mongoose = require('mongoose')
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
+    req.flash('warning', `a very fatal error`)
+    req.flash('error', `a very fatal error`)
+    req.flash('info', `a very fatal error`)
+    req.flash('success', `a very fatal error`)
+    req.flash('me  away', `a very fatal error`)
+    req.flash('info', `a very fatal error`)
     res.render('index', {name: req.name})
 }
 
@@ -6,7 +15,14 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' })
 }
 
-exports.createStore = (req, res) => {
-    console.log(req.body)
-    res.json(req.body)
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save()
+    req.flash('success', `Successfully created ${store.name}. Care to leave a Review?`)
+    res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find()
+    console.log(stores)
+    res.render('stores', { title: 'Stores', stores })
 }
