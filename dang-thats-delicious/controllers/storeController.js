83394fb9cbb92daf38df3b40a921af52a5f4423d
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-05-01 03:15:11 +0100

    create store.pug for each slug, add upload and resize image capabilities

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
+const multer = require('multer')
+const jimp = require('jimp')
+const uuid = require('uuid')
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/')
+        if (isPhoto) {
+            next(null, true)
+        } else {
+            next({
+                message: 'That filetype isn\'t allowed!'
+            }, false)
+        }
+    }
+}
 
 exports.homePage = (req, res) => {
     res.render('index', {
@@ -13,6 +30,22 @@ exports.addStore = (req, res) => {
     })
 }
 
+exports.upload = multer(multerOptions).single('photo')
+
+exports.resize = async (req, res, next) => {
+    if (!req.file) {
+        next()
+        return
+    }
+    const extension = req.file.mimetype.split('/')[1]
+    req.body.photo = `${uuid.v4()}.${extension}`
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // once we have written the photo to our filesystem, keep going!
+    next();
+}
+
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save()
     req.flash('success', `Successfully created ${store.name}. Care to leave a Review?`)
@@ -21,7 +54,7 @@ exports.createStore = async (req, res) => {
 
 exports.getStores = async (req, res) => {
     const stores = await Store.find()
-    console.log(stores)
+    // console.log(stores)
     res.render('stores', {
         title: 'Stores',
         stores
@@ -37,17 +70,27 @@ exports.editStore = async (req, res) => {
     res.render('editStore', {
         title: `Edit ${store.name}`,
         store
-    }) 
+    })
     // 3. Render out the edit form so the user can update their store
 }
 
 exports.updateStore = async (req, res) => {
-    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, 
-    {
+    const store = await Store.findOneAndUpdate({
+        _id: req.params.id
+    }, req.body, {
         new: true,
         runValidators: true
     }).exec()
-    console.log(store)
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`)
     res.redirect(`/stores/${store._id}/edit`)
+}
+
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug })
+    if (!store) { 
+        next()
+        return
+    }
+    res.render('store', { store, title: store.name })
 }
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,15 +2,29 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
 
-const { catchErrors } = require('../handlers/errorHandlers')
+const {
+  catchErrors
+} = require('../handlers/errorHandlers')
 
 // Do work here
-router.get('/stores', catchErrors(storeController.getStores));
 router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
-router.post('/add/:id', catchErrors(storeController.updateStore));
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore),
+)
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore));
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('')
